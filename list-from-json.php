142fa294aba4f5d66c1e4c7d829c5d5c584fa052
Stand Your Gists <76827290+FriendsAtDawn@users.noreply.github.com>
2025-09-21 08:57:22 -0300

    Create list-from-json.php

---
+++ list-from-json.php
@@ -0,0 +1,20 @@
+<?php
+// URL del JSON con la lista
+$jsonUrl = 'https://dominioremoto/dir.json';
+
+// Leer JSON desde URL
+$jsonContent = file_get_contents($jsonUrl);
+$fileNames = json_decode($jsonContent, true);
+
+$html = "<!DOCTYPE html><html><head><title>Listado PDF</title></head><body>\n";
+
+foreach ($fileNames as $file) {
+    $url = 'https://dominioremoto/' . $file;
+    $html .= "<h3>$file</h3>";
+    $html .= "<iframe src=\"$url\" width=\"600\" height=\"400\"></iframe><br><br>\n";
+}
+
+$html .= "</body></html>";
+
+file_put_contents('index.html', $html);
+echo "index.html generado correctamente desde JSON.\n";
