c3078ad07d185dc08119a7e313e47343c4e5b8a7
Stand Your Gists <76827290+FriendsAtDawn@users.noreply.github.com>
2025-09-21 08:17:12 -0300

    Update generate-iframe-list.php

--- generate-iframe-list.php
+++ generate-iframe-list.php
@@ -5,8 +5,8 @@ $file_names = ['98678.pdf','77878.pdf', '76778.pdf', '45789.pdf', '40571.pdf', '
 $html = "<!DOCTYPE html><html><head><title>Listado PDF</title><br /><link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"></head><body>\n";
 foreach ($file_names as $file) {
     $url = $base_url . $file;
-    $html .= "<h3>$file</h3>";
     $html .= "<iframe src=\"$url\" id=\"$file\" width=\"600\" height=\"400\"></iframe><br><br>\n";
+    $html .= "<h3>$file</h3>";
 }
 $html .= "</body></html>";
 
