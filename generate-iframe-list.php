029f0455866da8739d2aadddd80d58330faee450
Stand Your Gists <76827290+FriendsAtDawn@users.noreply.github.com>
2025-09-20 22:23:31 -0300

    Create generate-iframe-list.php

---
+++ generate-iframe-list.php
@@ -0,0 +1,14 @@
+<?php
+$base_url = 'https://repositorio.hstn.me/';
+$file_names = ['98678.pdf','77878.pdf', '76778.pdf', '45789.pdf', '40571.pdf', '101234.pdf']; // Puedes agregar o generar esta lista
+
+$html = "<!DOCTYPE html><html><head><title>Listado PDF</title></head><body>\n";
+foreach ($file_names as $file) {
+    $url = $base_url . $file;
+    $html .= "<h3>$file</h3>";
+    $html .= "<iframe src=\"$url\" width=\"600\" height=\"400\"></iframe><br><br>\n";
+}
+$html .= "</body></html>";
+
+file_put_contents('index.html', $html);
+echo "index.html generado correctamente.\n";
